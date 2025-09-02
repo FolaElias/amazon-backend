088015eb8fa0a81dc9a944121d40277958585a94
KvngSnow <unstablesteve22@gmail.com>
2025-09-02 11:03:09 +0100

    added foirst

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express()
+const product = require('./routes/products')
+
+mongoose.connect('mongodb://localhost/foodamazon')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connect to MongoDB...', err))
+
+
+app.use(express.json())
+app.use('/amazon/document/api/products', product)
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => console.log(`listening on port ${port}...`))
---
+++ routes/products.js
@@ -0,0 +1,56 @@
+
+const { Product, validate } = require("../models/product");
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async(req, res) => {
+    const product = await Product.find().sort('name')
+    res.send(product)
+});
+
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) return res.status(400).send('product with the given id not found');
+
+    res.send(product)
+});
+
+router.delete('/:id', async (req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id)
+    if (!product) return res.status(400).send('product with the given id not found');
+    
+    res.send(product)
+});
+
+
+router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(404).send(error.details[0].message) 
+    
+    let product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        description: req.body.description,
+        numberInStock: req.body.numberInStock
+    });
+
+    product = await product.save()
+    res.send(product)
+});
+
+
+router.put('/:id', async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.send(404).send(error.details[0].message) 
+    
+    const product = await Product.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true }, {image: req.body.image}, {price: req.body.price}, {description: req.body.description}, {numberInStock: req.body.numberInStock});
+    if (!product) return res.status(400).send('product with the given id not found');
+    
+    res.send(product)
+});
+
+
+module.exports = router
+
